1880f666b075c5de4cbc015068342a5264438d78
Quoc Doan <quocdoan@Quocs-Air.attlocal.net>
2016-03-17 00:51:50 -0700

    fundstarter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var http = require('http')
-var fs = require('fs')
-var file = 'index.html'
+var http = require('http');
+var fs = require('fs');
+var file = 'index.html';
 
 var server = http.createServer(function (req, res) {
     /* part 1a */
@@ -14,28 +14,30 @@ var server = http.createServer(function (req, res) {
     /* part 1b */
     /*
     var asynchRead = fs.readFileSync('index.html', 'utf8')
+    res.write(asyncRead)
+    res.end()
     */
     /* part 2 */
     fs.open(file, 'r', function(err, fd) {
         fs.fstat(fd, function(err, stats) {
-            var s = stats.size,
-                chunkSize = 512,
-                buffer = new Buffer(s),
-                byteRead = 0
-            console.log(file + " " + s)
-            while (byteRead < s) {
-                if((byteRead + chunkSize) > s) {
-                    chunkSize = (s - byteRead)
+            var buffer_size = stats.size,
+                chunk_size = 512,
+                buffer = new Buffer(buffer_size),
+                byte_read = 0;
+            console.log(file + " " + buffer_size);
+            while (byte_read < buffer_size) {
+                if((byte_read + chunk_size) > buffer_size) {
+                    chunk_size = (buffer_size - byte_read);
                 }
-                fs.read(fd, buffer, byteRead, chunkSize, byteRead)
-                byteRead += chunkSize
+                fs.read(fd, buffer, byte_read, chunk_size, byte_read);
+                byte_read += chunk_size;
             }
-            console.log(buffer.toString('utf8', 0, s))
-            res.write(buffer.toString('utf8', 0, s))
-            res.end()
-            })
-        })
-    })
+            console.log(buffer.toString('utf8', 0, buffer_size));
+            res.write(buffer.toString('utf8', 0, buffer_size));
+            res.end();
+        });
+    });
+});
 
-server.listen(process.env.PORT || 8080)
-console.log("http://127.0.0.1:8080/")
+server.listen(process.env.PORT || 8080);
+console.log("http://127.0.0.1:8080/");
